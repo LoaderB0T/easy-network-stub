b84fbb36af8ba9ff2cda3747cc7b8fa9b38236d6
Janik Schumacher <rndmb0t@gmail.com>
2022-04-03 03:28:45 +0200

    cleaned up typings

--- src/models/extract-route-params.ts
+++ src/models/extract-route-params.ts
@@ -8,22 +8,20 @@ export type FlattenUnion<T> = {} extends T
 
 export type ExtractRouteParams<T extends string> = T extends `${string}{${infer Param}}${infer Rest}`
   ? FlattenUnion<GetParam<Param> & ExtractRouteParams<Rest>>
-  : // ? GetParam<Param> & ExtractRouteParams<Rest>
-    {};
+  : {};
 
-// export type GetParam<T extends string> = T extends `${infer Param}?${string}`
-//   ? T extends `${string}:${infer ParamType}`
-//     ? { [K in Param]: TypeConverter<ParamType> | undefined }
-//     : { [K in Param]: string | undefined }
-//   : T extends `${infer Param}:${infer ParamType}`
-//   ? { [K in Param]: TypeConverter<ParamType> }
-//   : { [K in T]: string };
-export type GetParam<T extends string> = T extends `${infer Param}:${infer ParamType}`
-  ? T extends `${infer ParamWithoutQuestionmark}?:${string}`
-    ? { [K in ParamWithoutQuestionmark]: TypeConverter<ParamType> | undefined }
-    : { [K in Param]: TypeConverter<ParamType> }
-  : T extends `${infer ParamWithoutQuestionmark}?`
-  ? { [K in ParamWithoutQuestionmark]: string | undefined }
-  : { [K in T]: string };
+type GetParamOptional<T extends string> = T extends `${infer Param}?:${infer ParamType}`
+  ? { [K in Param]: TypeConverter<ParamType> | undefined }
+  : T extends `${infer Param}?`
+  ? { [K in Param]: string | undefined }
+  : never;
+
+type GetParamRequired<T extends string> = T extends `${infer Param}:${infer ParamType}`
+  ? { [K in Param]: TypeConverter<ParamType> }
+  : T extends `${infer Param}`
+  ? { [K in Param]: string }
+  : never;
+
+export type GetParam<T extends string> = T extends `${string}?${string}` ? GetParamOptional<T> : GetParamRequired<T>;
 
 export type TypeConverter<T extends string> = T extends 'number' ? number : T extends 'boolean' ? boolean : string;
