0bf19c729836f32372bde48f557acf170910d7e9
Janik Schumacher <rndmb0t@gmail.com>
2022-04-03 23:53:21 +0200

    .

--- src/helper/handle-request/fail-because-of-not-or-wrong-mocked-route.ts
+++ src/helper/handle-request/fail-because-of-not-or-wrong-mocked-route.ts
@@ -1,14 +1,19 @@
 import { Config } from '../../models/config';
 import { Request } from '../../models/request';
 
-export const failBecauseOfNotOrWrongMockedRoute = (req: Request, additionalErrorExplanation: string, config: Config) => {
+export const failBecauseOfNotOrWrongMockedRoute = (
+  req: Request,
+  additionalErrorExplanation: string,
+  config: Config,
+  stack: string
+) => {
   config.errorLogger({
     message: `Route not mocked: [${req.method}] ${req.url}${additionalErrorExplanation ? '\n' + additionalErrorExplanation : ''}`,
     method: req.method,
     request: req,
     registeredStubs: config.stubs,
     url: req.url,
-    stack: new Error().stack
+    stack
   });
   req.destroy();
   config.failer(`Route not mocked: [${req.method}] ${req.url}`);
