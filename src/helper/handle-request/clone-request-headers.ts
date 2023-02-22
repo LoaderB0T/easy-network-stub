cb3d62026047eeb65483a697668d8860c78e518d
Marc Schmidt <masch212@gmail.com>
2023-02-22 22:06:43 +0100

    fix: sonar cloud code smell

--- src/helper/handle-request/clone-request-headers.ts
+++ src/helper/handle-request/clone-request-headers.ts
@@ -2,11 +2,11 @@ import { Request } from '../../models/request.js';
 
 export const cloneRequestHeaders = (req: Request) => {
   const headers: { [key: string]: string | string[] } = {};
-  
+
   for (const key in req.headers) {
     const value = req.headers[key];
     if (value instanceof Array) {
-      headers[key] = [...(value as string[])];
+      headers[key] = [...value];
     } else {
       headers[key] = value;
     }
