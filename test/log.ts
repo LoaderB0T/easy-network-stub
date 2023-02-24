ebdc82c4ea4b1bdd5e68bae61465202080a10512
Janik Schumacher <rndmb0t@gmail.com>
2023-02-24 22:59:04 +0100

    yarn -> pnpm

--- test/log.ts
+++ test/log.ts
@@ -4,6 +4,7 @@ import { TestEasyNetworkStub } from './test-easy-network-stub.js';
 
 export const afterEachLog = async (testEasyNetworkStub: TestEasyNetworkStub) => {
   // TODO whenever possible: Add check if test is failed and only then log
+  // https://github.com/facebook/jest/issues/5292
   if (testEasyNetworkStub.lastError.message) {
     await normalConsole(() => {
       console.error(chalk.red(chalk.bold('This is the last error that occurred:')));
